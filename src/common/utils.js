574f5f8cd531a7627cd50de84246516c5992fa9e
Olexander <dushko1996@gmail.com>
2025-12-24 07:59:42 +0200

    include max in getRandomInt

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,6 +1,6 @@
 export const roundTo = (value, decimals) => Math.round(value * (10 ** decimals)) / (10 ** decimals);
 export const normalize = (value, denominator) => Math.round(value / denominator) * denominator;
-export const getRandomInt = (min, max) => Math.floor(Math.random() * ((max - min)) + min); // max excluded
+export const getRandomInt = (min, max) => Math.floor(Math.random() * ((max - min) + 1) + min); 
 export const deepCopy = (obj) => obj.map(p => ({ ...p}));
 
 export function emulateClick(el) {
